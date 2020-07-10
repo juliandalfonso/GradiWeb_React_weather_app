fb3f6c2d749139c6ac3a0fa6dd9f1ae557f7070c
Julian Alfonso <juliandavid.alfonso@utp.edu.co>
2020-07-09 22:09:09 -0500

    se ha completado satisfactoriamente la recepcion de la información desde la API y por medio de un formulario se muestra la información de la ciudad y país solicitados, se muestra la temperatura, humedad, velocidad del viento, etc

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,16 @@
-import React, {Component} from 'react';
+import React, {
+    Component
+} from 'react';
 
 
 import WeatherInfo from './components/WeatherInfo';
 import WeatherForm from './components/WeatherForm';
 
-import {WEATHER_KEY} from './keys';
+import {
+    WEATHER_KEY
+} from './keys';
 
-class App extends Component 
-{
+class App extends Component {
     state = {
         temperature: '',
         description: '',
@@ -18,36 +21,59 @@ class App extends Component
         error: null
     }
 
-    getWeather= async e=> {
+    getWeather = async e => {
         e.preventDefault();
-        const {city,country} = e.target.elements;
-        const cityValue = city.value; 
-        const countryValue = country.value; 
-        console.log(cityValue,countryValue);
+        const {
+            city,
+            country
+        } = e.target.elements;
+        const cityValue = city.value;
+        const countryValue = country.value;
 
-        const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${city.value},${country.value}&appid=${WEATHER_KEY}`;
+        if(cityValue && countryValue)
+        {
+            const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${city.value},${country.value}&appid=${WEATHER_KEY}&units=metric`;
 
         const response = await fetch(API_URL);
 
         const data = await response.json();
 
-        console.log(data,'funciona');
+        this.setState({
+            temperature: data.main.temp,
+            description: data.weather[0].description,
+            humidity: data.main.humidity,
+            wind_speed: data.wind.speed,
+            city: data.name,
+            country: data.sys.country,
+            error: null
+        });
+        }
+        else
+        {
+            this.setState({error: 'Enter a city or a country'})
+        }
+        
 
     }
     //key efffc88fcf7dcac6b4034914fa53bf4c
 
 
-    render() 
-    {
-        return(
-            <div className="container p-4">
-                <div className="row">
-                    <div className="col-md-6">
-                        <WeatherForm getWeather={this.getWeather}/>
-                        <WeatherInfo/>
-                    </div>
-                </div>
-            </div>
+    render() {
+        return ( <
+            div className = "container p-4" >
+            <
+            div className = "row" >
+            <
+            div className = "col-md-6" >
+            <
+            WeatherForm getWeather = {
+                this.getWeather
+            }
+            /> <WeatherInfo {...this.state}/ >
+            <
+            /div> < /
+            div > <
+            /div>
 
         )
     }
--- src/app/components/WeatherInfo.js
+++ src/app/components/WeatherInfo.js
@@ -2,9 +2,43 @@ import React from 'react';
 
 const WeatherInfo = props => 
 {
+    console.log(props);
     return(
+
         <div>
-            WeatherInfo
+            {
+            props.error &&
+            <div className="alert alert-danger">
+                <p>{props.error}</p>
+            </div>
+        }
+        {
+            props.temperature ?
+
+            <div className="card card-body">
+           <p>
+               Location: {props.city}, {props.country}
+           </p>
+           <p>
+               Temperature: {props.temperature}°C, {props.description}
+           </p>
+           <p>
+               Humidity: {props.humidity}
+           </p>
+           <p>
+               Wind Speed: {props.wind_speed}
+           </p>
+        </div>
+
+        :
+        <div className="card card-body">
+            <p>No requests yet</p>
+            </div>
+            
+
+        }
+        
+
         </div>
     )
 }
